b614c8143ea83d65f9d258235f81dbee5ee9ac9e
Samuel Yin <wsgddjy@live.com>
2024-02-19 21:37:43 +1000

    194. Shuffling Answers & Adding Quiz Logic

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,46 +1,53 @@
 import QUESTIONS from "../questions.ts";
 import { useState } from "react";
+import QuizOverImg from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
+
   const activeQuestionIndex = userAnswers.length;
+  const quizOver = activeQuestionIndex === QUESTIONS.length;
   function handleSelectAnswer(selectedAnswer: string) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+  if (quizOver) {
+    return (
+      <div id={"summary"}>
+        <img src={QuizOverImg} alt={"quiz over"} />
+        <div>overview</div>
+        <ul id={"answers"}>
+          {userAnswers.map((answer, index) => (
+            <li key={index} className={"answer"}>
+              {answer}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <>
       <main>
-        {activeQuestionIndex < QUESTIONS.length ? (
-          <div id={"quiz"}>
-            <p>progress bar</p>
-            <div id={"question"}>
-              <h2 id={"question-overview"}>
-                {QUESTIONS[activeQuestionIndex].text}
-              </h2>
-              <ul id={"answers"}>
-                {QUESTIONS[activeQuestionIndex].answers.map((answers) => (
-                  <li key={answers} className={"answer"}>
-                    <button onClick={() => handleSelectAnswer(answers)}>
-                      {answers}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ) : (
+        <div id={"quiz"}>
           <div id={"question"}>
-            <div>overview</div>
+            <h2 id={"question-overview"}>
+              {QUESTIONS[activeQuestionIndex].text}
+            </h2>
             <ul id={"answers"}>
-              {userAnswers.map((answer, index) => (
-                <li key={index} className={"answer"}>
-                  {answer}
+              {QUESTIONS[activeQuestionIndex].answers.map((answers) => (
+                <li key={answers} className={"answer"}>
+                  <button onClick={() => handleSelectAnswer(answers)}>
+                    {answers}
+                  </button>
                 </li>
               ))}
             </ul>
           </div>
-        )}
+        </div>
       </main>
     </>
   );
